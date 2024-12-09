0f2b36952ca2a4eeef2853e75ce51a2ed22249ec
Abdullah Mashaan <eng.aalenezi@gmail.com>
2024-12-09 18:25:22 +0300

    Abdullah Mashaan is here

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-
 app.get("/", (req, res) => {
   const names = [];
   names.push("Abdullah");
   names.push("Moudhi");
-  names.push("Omar")
-   names.push("Dawood")
+  names.push("Omar");
+  names.push("Dawood");
   names.push("Hey there! This is Sultan!");
+  names.push("Abdullah Mashaan");
   res.send(names);
-
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
