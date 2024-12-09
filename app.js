4c6eaebe27a667ca7ae7875cf0d3b91d8c3a2bef
mohamedshaban <sarahmama@moe.edu.kw>
2024-12-09 18:24:23 +0300

    SarahM added

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-
 app.get("/", (req, res) => {
   const names = [];
   names.push("Abdullah");
   names.push("Moudhi");
-  names.push("Omar")
-   names.push("Dawood")
+  names.push("Omar");
+  names.push("Dawood");
+  names.push("SarahM");
   names.push("Hey there! This is Sultan!");
   res.send(names);
-
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
