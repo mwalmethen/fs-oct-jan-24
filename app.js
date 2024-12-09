77a4cadb2b6aaf05f0d92e7eacfd8bd8faa647f5
MrHasak99 <hamad_alkhalaf_99@hotmail.com>
2024-12-09 18:38:13 +0300

    Hamad

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.get("/", (req, res) => {
   names.push("Omar");
   names.push("Dawood");
   names.push("Sarah");
-   names.push("Abdullah Al Abbas");
-   names.push("Abdullah Mashaan");
-
+  names.push("Abdullah Al Abbas");
+  names.push("Abdullah Mashaan");
+  names.push("Hamad AlKhalaf")
   names.push("Hey there! This is Sultan!");
 
   res.send(names);
