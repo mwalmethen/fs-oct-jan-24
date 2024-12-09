b4aa1541fa5f64986a3af0312e150a85fd290dfa
SND Dev <sws.indiedev@gmail.com>
2024-12-09 18:00:19 +0300

    sultan added

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-    const names = []
-    names.push("Abdullah")
-    res.send(names);
+app.get("/", (req, res) => {
+  const names = [];
+  names.push("Abdullah");
+  names.push("Hey there! This is Sultan!");
+  res.send(names);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
